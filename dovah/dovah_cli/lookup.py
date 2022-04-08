fd36bee22c24a3151fcae15e1a3e5f16680b6937
PatOConnor <patoconnorma@gmail.com>
2022-04-07 20:39:46 -0400

    wrote lookup

--- dovah/dovah_cli/lookup.py
+++ dovah/dovah_cli/lookup.py
@@ -11,66 +11,95 @@ from .skyrimdata.skyrimskills import skyrim_skills
 from .skyrimdata.skyrimalchemy import ingredients, effects
 from .skyrimdata.skyrimlocations import skyrim_locations
 
-
-def catalog_reference(category):
-    if category == 'commands':
-        targetdict = skyrim_commands
-    elif category == 'av':
-        targetdict = skyrim_avs
-    elif category == 'enchantments':
-        targetdict = skyrim_enchantments
-    elif category == 'item':
-        targetdict = skyrim_items
-    elif category == 'npc':
-        targetdict = skyrim_npcs
-    elif category == 'perk':
-        targetdict = skyrim_perks
-    elif category == 'quest':
-        targetdict = skyrim_quests
-    elif category == 'spell':
-        targetdict = skyrim_spells
-    elif category == 'weather':
-        targetdict = skyrim_weather
-    elif category == 'skill':
-        targetdict = skyrim_skills
-    elif category == 'ingredients':
-        targetdict = ingredients
-    elif category == 'effects':
-        targetdict = effects
-    elif category == 'locations':
-        targetdict = skyrim_locations
-    else:
-        raise ValueError
-    return targetdict
-
-#small user text interface
-def user_lookup(category): 
-    #grabs dictionary to pick through
-    catalog = catalog_reference(category)
-    while (True):
-        start_str = input("Enter the first letters of the desired "+category+": ")
-        for i in catalog:
-            if catalog[i][0].lower().startswith(start_str):
-                print(i, catalog[i])
-        word = input('which one is being added? ')
-        k = catalog_lookup
-        if k != -1:
-            return catalog[k]
-
-#returns the key of the item that has the argument string in the first element
-def catalog_lookup(lookupstr, category):
-    catalog = catalog_reference(category)
-    #first, check if it itself is a number and continue on if it is not
-    try:
-        lookupstr = int(lookupstr)
-        if lookupstr in skyrim_commands:
-            return lookupstr
-        else:
-            return -1
-    except ValueError:
+class Lookup:
+    #this class is not designed to have objects made of it
+    #this is for dependency injection while testing
+    def __init__(self):
         pass
-    #nif not a number, check if it matches the name of a catalog entry
-    for k in skyrim_commands:
-        if skyrim_commands[k]['NAME'].lower().strip() == lookupstr.lower().strip():
-            return k
-    return -1
+
+    #returns the text to add to a command based on the 
+    #picks the first string that matches
+    def lookup(lookupstr, category):
+        catalog = Lookup.catalog_reference(category)
+        #check if it matches the name of a catalog entry
+        for k in catalog:
+            if catalog[k]['NAME'].lower().strip() == lookupstr.lower().strip():
+                lookup_item = catalog[k]
+        category = category.lower().strip()
+        if category in ['alcheffect', 'alcheffects', 'av', 'avs', 'attribute', 'attributes', 
+                        'ench', 'enchant', 'enchantment', 'enchantments', 'ingr', 'ingredient', 
+                        'ingredients', 'item', 'items', 'loc', 'location', 'locations', 'perk', 
+                        'perks', 'quests', 'quests', 'spell', 'spells', 'weather']:
+            return lookup_item['CODE']
+        elif category in ['fac', 'faction', 'factions']:
+            return lookup_item['INFO']['Form ID']
+        elif category in ['npc', 'npcs']:
+            return lookup_item['REF_ID']
+
+
+        
+
+    #returns the key of the item that has the argument string in the first element
+    def catalog_lookup(lookupstr, category):
+        catalog = DovahLookup.catalog_reference(category)
+        #first, check if it itself is a number and continue on if it is not
+        try:
+            lookupstr = int(lookupstr)
+            if lookupstr in skyrim_commands:
+                return lookupstr
+            else:
+                return -1
+        except ValueError:
+            pass
+        #nif not a number, check if it matches the name of a catalog entry
+        for k in catalog:
+            if catalog[k]['NAME'].lower().strip() == lookupstr.lower().strip():
+                return k
+        return -1
+
+    def catalog_reference(category):
+        if category == 'commands':
+            targetdict = skyrim_commands
+        elif category == 'av':
+            targetdict = skyrim_avs
+        elif category == 'enchantments':
+            targetdict = skyrim_enchantments
+        elif category == 'item':
+            targetdict = skyrim_items
+        elif category == 'npc':
+            targetdict = skyrim_npcs
+        elif category == 'perk':
+            targetdict = skyrim_perks
+        elif category == 'quest':
+            targetdict = skyrim_quests
+        elif category == 'spell':
+            targetdict = skyrim_spells
+        elif category == 'weather':
+            targetdict = skyrim_weather
+        elif category == 'skill':
+            targetdict = skyrim_skills
+        elif category == 'ingredients':
+            targetdict = ingredients
+        elif category == 'effects':
+            targetdict = effects
+        elif category == 'locations':
+            targetdict = skyrim_locations
+        else:
+            raise ValueError
+        return targetdict
+
+    #small user text interface
+    def user_lookup(category): 
+        #grabs dictionary to pick through
+        catalog = DovahLookup.self.cmd_maker.add_cmd(['additem', 'player', 'f', 1])#number inputted as a numbercatalog_reference(category)
+        while (True):
+            start_str = input("Enter the first letters of the desired "+category+": ")
+            for i in catalog:
+                if catalog[i][0].lower().startswith(start_str):
+                    print(i, catalog[i])
+            word = input('which one is being added? ')
+            k = DovahLookup.catalog_lookup()
+            if k != -1:
+                return catalog[k]
+
+   
--- dovah/tests/cmdtest.py
+++ dovah/tests/cmdtest.py
@@ -2,17 +2,19 @@
    actually function in-game in skyrim"""
 # This line does NOT work and is not intended for the final product
 # it's for use while coding to enable intellisense when combined with a type hint.
+from cgitb import lookup
 from ..dovah_cli.commandmaker import CommandMaker
+from ..dovah_cli.lookup import Lookup
 
 class CmdTest:
-    def __init__(self, cmd_maker_object:CommandMaker):
+    def __init__(self, cmd_maker_object:CommandMaker, lookup_object:Lookup):
         self.cmd_maker = cmd_maker_object
-
+        self.lookup = lookup_object.lookup
 
     def run(self):
         self.complete_commands()
         self.incomplete_commands()
-        self.bad_input_commands()
+        self.broken_commands()
     
     def complete_commands(self):
         #does not take a parameter
@@ -25,32 +27,44 @@ class CmdTest:
     def incomplete_commands(self):
         #blank command - shuold place user right at beginning of "add command" screen
         self.cmd_maker.add_cmd()
-        
         self._incomplete_toggle_commands()
 
-    def bad_input_commands(self):
-        pass
+    def broken_commands(self):
+        self._broken_toggle_commands()
+
+
+    #############################################################
+    #   COMPLETE COMMANDS BLOCK: THESE SHOULD WORK W/O INPUT    #
+    #############################################################
 
     def _complete_toggle_commands(self):
-        """No User Interaction"""
-        #writes command with no argument
-        self.cmd_maker.add_cmd(['0'])
-        self.cmd_maker.add_cmd(['animcam'])
+        #writes toggle command with no argument
+        self.cmd_maker.add_cmd(['0'])#read by code
+        self.cmd_maker.add_cmd(['animcam'])#read by name
         self.cmd_maker.add_cmd(['0', 'foo'])#should ignore extra input
-        self.cmd_maker.add_cmd(['tcl', '1A6A4'])
-        self.cmd_maker.add_cmd(['tcl', None])
+        #targeted toggle commands
+        self.cmd_maker.add_cmd(['tcl', '1A6A4'])#Nazeem
+        self.cmd_maker.add_cmd(['tcl', '']) #no target
+        #toggle commands with arguments
+        self.cmd_maker.add_cmd(['tmm', '1', '0', '1'])
+        #toggle command with automatically-assigned argument
+        self.cmd_maker.add_cmd([['tfc']])
+        self.cmd_maker.add_cmd(['tfc', '1'])
         
     
     def _complete_targeted_commands(self):
-        self.cmd_maker.add_cmd()
-        self.cmd_maker.add_cmd()
-        self.cmd_maker.add_cmd()
-        self.cmd_maker.add_cmd()
-        self.cmd_maker.add_cmd()
-        self.cmd_maker.add_cmd()
+        #additem
+        self.cmd_maker.add_cmd(['24', 'player', 'f', '1'])
+        self.cmd_maker.add_cmd(['additem', 'player', 'f', '1'])
+        self.cmd_maker.add_cmd(['additem', 'player', 'f', 1])#number inputted as an int
+        self.cmd_maker.add_cmd(['additem', 'player', 'f', 1.0])#number inputted as a float
+        
+        self.cmd_maker.add_cmd(['additem', 'player', self.lookup('gold')], '1')#using lookup
+        self.cmd_maker.add_cmd(['additem', self.lookup('nazeem'), self.lookup('gold'), 1])
+        
         
 
-    def _complete_untargeted_commands():
+    def _complete_untargeted_commands(self):
         self.cmd_maker.add_cmd()
         self.cmd_maker.add_cmd()
         self.cmd_maker.add_cmd()
@@ -62,6 +76,9 @@ class CmdTest:
         self.cmd_maker.add_cmd()
         self.cmd_maker.add_cmd()
 
+    #############################################################
+    #   INCOMPLETE COMMANDS BLOCK: THESE SHOULD ASK FOR INPUT   #
+    #############################################################
 
     def _incomplete_toggle_commands(self):
             """User Interaction"""
@@ -69,3 +86,10 @@ class CmdTest:
             self.cmd_maker.add_cmd('tcl')
             self.cmd_maker.add_cmd('tcai')
 
+
+    #############################################################
+    #   BAD INPUT COMMANDS BLOCK: THESE SHOULD THROW EXCEPTIONS #
+    #############################################################
+
+    def _broken_toggle_commands(self):
+        self.cmd_maker.add_cmd(['tcl', None]) #None should be empty string
